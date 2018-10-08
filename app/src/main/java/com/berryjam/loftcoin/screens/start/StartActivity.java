5179a4648d25213e2c31f9313b6e8050b22f0868
Mozgus <retailnet@rambler.ru>
2018-10-08 12:29:59 +0300

    #2 Blank activities created

--- app/src/main/java/com/berryjam/loftcoin/screens/start/StartActivity.java
+++ app/src/main/java/com/berryjam/loftcoin/screens/start/StartActivity.java
@@ -1,13 +1,15 @@
-package com.berryjam.loftcoin;
+package com.berryjam.loftcoin.screens.start;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.berryjam.loftcoin.R;
+
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
     }
 }
