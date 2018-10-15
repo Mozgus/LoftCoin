cecd49003c7f4710f59fe80eb32943345830453f
Mozgus <retailnet@rambler.ru>
2018-10-15 17:23:15 +0300

    #3 Start screen planets animation

--- app/src/main/java/com/berryjam/loftcoin/screens/start/StartActivity.java
+++ app/src/main/java/com/berryjam/loftcoin/screens/start/StartActivity.java
@@ -1,15 +1,23 @@
 package com.berryjam.loftcoin.screens.start;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 import com.berryjam.loftcoin.App;
 import com.berryjam.loftcoin.R;
 import com.berryjam.loftcoin.data.api.Api;
 import com.berryjam.loftcoin.data.prefs.Prefs;
 import com.berryjam.loftcoin.screens.main.MainActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class StartActivity extends AppCompatActivity implements StartView {
 
@@ -23,16 +31,46 @@ public class StartActivity extends AppCompatActivity implements StartView {
     private Api api;
     private Prefs prefs;
 
+    @BindView(R.id.start_top_corner)
+    ImageView topCorner;
+    @BindView(R.id.start_bottom_corner)
+    ImageView bottomCorner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        ButterKnife.bind(this);
 
         api = ((App) getApplication()).getApi();
         prefs = ((App) getApplication()).getPrefs();
         presenter = new StartPresenterImpl(api, prefs);
         presenter.attachView(this);
         presenter.loadRate();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startAnimations();
+    }
+
+    private void startAnimations() {
+        ObjectAnimator innerAnimator = ObjectAnimator.ofFloat(topCorner, "rotation", 0, 360);
+        innerAnimator.setDuration(30000);
+        innerAnimator.setRepeatMode(ValueAnimator.RESTART);
+        innerAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        innerAnimator.setInterpolator(new LinearInterpolator());
+
+        ObjectAnimator outerAnimator = ObjectAnimator.ofFloat(bottomCorner, "rotation", 0, -360);
+        outerAnimator.setDuration(60000);
+        outerAnimator.setRepeatMode(ValueAnimator.RESTART);
+        outerAnimator.setRepeatCount(ValueAnimator.INFINITE);
+        outerAnimator.setInterpolator(new LinearInterpolator());
+
+        AnimatorSet set = new AnimatorSet();
+        set.play(innerAnimator).with(outerAnimator);
+        set.start();
     }
 
     @Override
