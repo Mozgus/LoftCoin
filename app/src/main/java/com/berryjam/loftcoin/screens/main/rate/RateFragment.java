8649503ec92f911d9b02c5191f5d122db2f5d7b6
Mozgus <retailnet@rambler.ru>
2018-10-16 16:20:12 +0300

    #4 Swipe to refresh mechanic in the rate screen

--- app/src/main/java/com/berryjam/loftcoin/screens/main/rate/RateFragment.java
+++ app/src/main/java/com/berryjam/loftcoin/screens/main/rate/RateFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -30,6 +31,8 @@ public class RateFragment extends Fragment implements RateView {
     RecyclerView recycler;
     @BindView(R.id.rate_toolbar)
     Toolbar toolbar;
+    @BindView(R.id.rate_refresh)
+    SwipeRefreshLayout refresh;
 
     private RatePresenter presenter;
     private RateAdapter adapter;
@@ -66,6 +69,13 @@ public class RateFragment extends Fragment implements RateView {
         recycler.setHasFixedSize(true);
         recycler.setAdapter(adapter);
 
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                presenter.onRefresh();
+            }
+        });
+
         presenter.attachView(this);
         presenter.getRate();
     }
@@ -84,6 +94,7 @@ public class RateFragment extends Fragment implements RateView {
 
     @Override
     public void setRefreshing(Boolean refreshing) {
+        refresh.setRefreshing(refreshing);
     }
 
     @Override
