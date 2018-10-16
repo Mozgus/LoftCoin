93b2a1afe09d96562aadf94e8a62d54bbbd59d1f
Mozgus <retailnet@rambler.ru>
2018-10-16 16:07:14 +0300

    #4 Fill rate screen content

--- app/src/main/java/com/berryjam/loftcoin/screens/main/rate/RateFragment.java
+++ app/src/main/java/com/berryjam/loftcoin/screens/main/rate/RateFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,10 +26,13 @@ import butterknife.Unbinder;
 
 public class RateFragment extends Fragment implements RateView {
 
+    @BindView(R.id.rate_recycler)
+    RecyclerView recycler;
     @BindView(R.id.rate_toolbar)
     Toolbar toolbar;
 
     private RatePresenter presenter;
+    private RateAdapter adapter;
     private Unbinder unbinder;
 
     @Override
@@ -41,6 +46,8 @@ public class RateFragment extends Fragment implements RateView {
         Api api = ((App) getActivity().getApplication()).getApi();
         Prefs prefs = ((App) getActivity().getApplication()).getPrefs();
         presenter = new RatePresenterImpl(api, prefs);
+        adapter = new RateAdapter(prefs);
+        adapter.setHasStableIds(true);
     }
 
     @Override
@@ -54,6 +61,11 @@ public class RateFragment extends Fragment implements RateView {
         unbinder = ButterKnife.bind(this, view);
 
         toolbar.setTitle(R.string.rate_screen_title);
+
+        recycler.setLayoutManager(new LinearLayoutManager(requireContext()));
+        recycler.setHasFixedSize(true);
+        recycler.setAdapter(adapter);
+
         presenter.attachView(this);
         presenter.getRate();
     }
@@ -67,6 +79,7 @@ public class RateFragment extends Fragment implements RateView {
 
     @Override
     public void setCoins(List<Coin> coins) {
+        adapter.setCoins(coins);
     }
 
     @Override
