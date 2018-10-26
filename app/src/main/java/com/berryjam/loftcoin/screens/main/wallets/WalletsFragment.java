0d78d1c095bfb67f80b5f1f4d9241ba808d18277
Mozgus <retailnet@rambler.ru>
2018-10-26 21:43:18 +0300

    #9 Wallet cards transformation

--- app/src/main/java/com/berryjam/loftcoin/screens/main/wallets/WalletsFragment.java
+++ app/src/main/java/com/berryjam/loftcoin/screens/main/wallets/WalletsFragment.java
@@ -83,6 +83,7 @@ public class WalletsFragment extends Fragment implements CurrenciesBottomSheetLi
         walletsPager.setPageMargin(-pageMargin);
         walletsPager.setOffscreenPageLimit(5);
         walletsPager.setAdapter(walletsPagerAdapter);
+        walletsPager.setPageTransformer(false, new ZoomOutPageTransformer());
 
         transactionsRecycler.setLayoutManager(new LinearLayoutManager(requireContext()));
         transactionsRecycler.setHasFixedSize(true);
@@ -163,6 +164,32 @@ public class WalletsFragment extends Fragment implements CurrenciesBottomSheetLi
         Point size = new Point();
         display.getSize(size);
         return size.x;
+    }
+
+    private class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        public void transformPage(@NonNull View view, float position) {
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+
     }
 
 }
